e03e761d8dc8aaa5633e85bfcc69ef7f5120fff6
kthoons <kth9987992@gmail.com>
2021-08-30 16:41:15 +0900

    Bobble_pointer_aim_edit

--- PUZZLEBOBBLEWORKSPACE/1_frame.py
+++ PUZZLEBOBBLEWORKSPACE/1_frame.py
@@ -7,3 +7,13 @@ screeen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Puzzle Bobble")
 clock = pygame.time.Clock()
 
+running = True
+while running:
+    clock.tick(60) # FPS 60 으로 설정
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
+
