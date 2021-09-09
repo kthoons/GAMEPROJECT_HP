59fda489b792234cd71a1422ee323de9a3e425cf
kthoons <kth9987992@gmail.com>
2021-09-09 17:07:28 +0900

    "내가 만드는 보블게임 pointer aim 까지"

--- PUZZLEBOBBLEWORKSPACE/do_myself.py
+++ PUZZLEBOBBLEWORKSPACE/do_myself.py
@@ -1,9 +1,90 @@
 import os
 import pygame
 
+# 버블 클래스 생성
+class Bubble(pygame.sprite.Sprite):
+    def __init__(self, image, color, position):
+        super().__init__()
+        self.image = image
+        self.color = color
+        self.rect = image.get_rect(center=position)
+
+# 발사대 클래스 생성
+class Pointer(pygame.sprite.Sprite):
+    def __init__(self, image, position, angle):
+        super().__init__()   
+        self.image = image       
+        self.rect = image.get_rect(center=position)
+        self.angle = angle
+        self.original_image = image
+        self.position = position
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+    # 회전
+    def rotate(self, angle):
+        self.angle += angle
+
+        if self.angle > 170:
+            self.angle = 170
+        elif self.angle < 10:
+            self.angle = 10
+
+        self.image = pygame.transform.rotozoom(self.original_image, self.angle, 1)
+        self.rect = self.image.get_rect(center=self.position)
+
+# 맵 만들기
+def setup():
+    global map
+    map = [
+        # ["R", "R", "Y", "Y", "B", "B", "G", "G"]
+        list("RRYYBBGG"), 
+        list("RRYYBBG/"), # / : 버블이 위치할 수 없는 곳
+        list("BBGGRRYY"),
+        list("BGGRRYY/"),
+        list("........"),
+        list("......./"),
+        list("........"),
+        list("......./"),
+        list("........"),
+        list("......./"),
+        list("........")
+    ]
+
+    for row_idx, row in enumerate(map):
+        for col_idx, col in enumerate(row):
+            if col in [".", "/"]:
+                continue
+
+            position = get_bubble_position(row_idx, col_idx)
+            image = get_bubble_image(col)
+            bubble_group.add(Bubble(image, col, position))
+
+def get_bubble_position(row_idx, col_idx):
+    pos_x = col_idx * CELL_SIZE + (BUBBLE_WIDTH // 2)   
+    pos_y = row_idx * CELL_SIZE + (BUBBLE_HEIGHT // 2)
+    if row_idx % 2 == 1:
+        pos_x += CELL_SIZE // 2 
+    return pos_x, pos_y
+
+def get_bubble_image(color):    
+    if color == "B":
+        return bubble_images[0]
+    elif color == "Y":
+        return bubble_images[1]
+    elif color == "G":
+        return bubble_images[2]
+    elif color == "P":
+        return bubble_images[3]
+    elif color == "R":
+        return bubble_images[4]
+    else:
+        return bubble_images[-1]
+
+
 pygame.init()
-screen_height = 720
 screen_width = 448
+screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Puzzle Bobble")
 clock = pygame.time.Clock()
@@ -13,7 +94,7 @@ current_path = os.path.dirname(__file__)
 background = pygame.image.load(os.path.join(current_path, "background.png"))
 
 # 구슬 만들기
-bubbles = [
+bubble_images = [
     pygame.image.load(os.path.join(current_path, "blue.png")),
     pygame.image.load(os.path.join(current_path, "yellow.png")),
     pygame.image.load(os.path.join(current_path, "green.png")),
@@ -22,23 +103,34 @@ bubbles = [
     pygame.image.load(os.path.join(current_path, "black.png"))
 ]
 
+# 발사대 만들기
+pointer_image = pygame.image.load(os.path.join(current_path, "pointer.png"))
+pointer = Pointer(pointer_image, (screen_width // 2, 624), 90)
 
 
 
 
 
+# 게임 관련 변수
+CELL_SIZE = 56
+BUBBLE_WIDTH = 56
+BUBBLE_HEIGHT = 62
 
+# 포인터 관련 변수
+to_angle_left = 0
+to_angle_right = 0
+angle_speed = 1.5
 
-
-
-
-
+map = []
+bubble_group = pygame.sprite.Group()
+setup()
 
 ############################################################################################################
 ############################################################################################################
 ############################################################################################################
 ############################################################################################################
 ############################################################################################################
+ 
 
 running = True
 while running:
@@ -47,8 +139,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_angle_left += angle_speed
+            elif event.key == pygame.K_RIGHT:
+                to_angle_right -= angle_speed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                to_angle_left = 0
+            elif event.key == pygame.K_RIGHT:
+                to_angle_right = 0
+
+            
     screen.blit(background, (0, 0))
+    bubble_group.draw(screen)
+    pointer.rotate(to_angle_left + to_angle_right)
+    pointer.draw(screen)
     pygame.display.update()
 
 pygame.quit()
